98ebb4d6742985ba71872bf38fe9b8da230cfc30
Milos Subotic <milos.subotic.sm@gmail.com>
2015-08-26 13:41:17 +0200

    Working.

---
+++ SDK/SDK_Workspace/test_mad/src/main.c
@@ -0,0 +1,179 @@
+/*
+ * Copyright (c) 2009-2012 Xilinx, Inc.  All rights reserved.
+ *
+ * Xilinx, Inc.
+ * XILINX IS PROVIDING THIS DESIGN, CODE, OR INFORMATION "AS IS" AS A
+ * COURTESY TO YOU.  BY PROVIDING THIS DESIGN, CODE, OR INFORMATION AS
+ * ONE POSSIBLE   IMPLEMENTATION OF THIS FEATURE, APPLICATION OR
+ * STANDARD, XILINX IS MAKING NO REPRESENTATION THAT THIS IMPLEMENTATION
+ * IS FREE FROM ANY CLAIMS OF INFRINGEMENT, AND YOU ARE RESPONSIBLE
+ * FOR OBTAINING ANY RIGHTS YOU MAY REQUIRE FOR YOUR IMPLEMENTATION.
+ * XILINX EXPRESSLY DISCLAIMS ANY WARRANTY WHATSOEVER WITH RESPECT TO
+ * THE ADEQUACY OF THE IMPLEMENTATION, INCLUDING BUT NOT LIMITED TO
+ * ANY WARRANTIES OR REPRESENTATIONS THAT THIS IMPLEMENTATION IS FREE
+ * FROM CLAIMS OF INFRINGEMENT, IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ */
+
+/*
+ * helloworld.c: simple test application
+ *
+ * This application configures UART 16550 to baud rate 9600.
+ * PS7 UART (Zynq) is not initialized by this application, since
+ * bootrom/bsp configures it to baud rate 115200
+ *
+ * ------------------------------------------------
+ * | UART TYPE   BAUD RATE                        |
+ * ------------------------------------------------
+ *   uartns550   9600
+ *   uartlite    Configurable only in HW design
+ *   ps7_uart    115200 (configured by bootrom/bsp)
+ */
+
+
+#include <stdio.h>
+#include "platform.h"
+#include "xparameters.h"
+#include "xil_types.h"
+
+#include "pff.h"
+#include "mad.h"
+
+#define INPUT_BUFFER_LEN 64
+
+static int in_chunk_cnt = 0;
+static int out_chunk_cnt = 0;
+
+static enum mad_flow input_fun(void *data, struct mad_stream *stream) {
+	FRESULT rc;
+	WORD br;
+	BYTE buff[INPUT_BUFFER_LEN];
+
+	(void)data;
+
+	xil_printf("in_chunk_cnt = %d\n", in_chunk_cnt);
+
+	// Read buffer.
+	rc = pf_read(buff, sizeof(buff), &br);
+	// Error or end of file.
+	if (rc || !br){
+		return MAD_FLOW_STOP;
+	}
+
+	mad_stream_buffer(stream, buff, sizeof(buff));
+
+	xil_printf("in_chunk_cnt = %d\n", in_chunk_cnt);
+	in_chunk_cnt++;
+
+	return MAD_FLOW_CONTINUE;
+}
+
+static enum mad_flow output_fun(
+	void *data,
+	struct mad_header const *header,
+	struct mad_pcm *pcm
+) {
+	unsigned int nchannels, nsamples;
+	mad_fixed_t const *left_ch, *right_ch;
+
+	/* pcm->samplerate contains the sampling frequency */
+
+	nchannels = pcm->channels;
+	nsamples = pcm->length;
+	left_ch = pcm->samples[0];
+	right_ch = pcm->samples[1];
+
+	xil_printf("out_chunk_cnt = %d\n", out_chunk_cnt);
+	out_chunk_cnt++;
+
+	return MAD_FLOW_CONTINUE;
+}
+
+static
+enum mad_flow error_fun(
+	void *data,
+	struct mad_stream *stream,
+	struct mad_frame *frame
+){
+	(void)data;
+
+	xil_printf(
+		"decoding error 0x%04x (%s) at byte offset %u\n",
+		stream->error, mad_stream_errorstr(stream),
+		INPUT_BUFFER_LEN*in_chunk_cnt + stream->this_frame
+	);
+
+	/* return MAD_FLOW_BREAK here to stop decoding (and propagate an error) */
+
+	return MAD_FLOW_CONTINUE;
+}
+
+#include "decoder.h"
+
+int main (void)
+{
+	FATFS fatfs;			/* File system object */
+	FRESULT rc;
+
+    init_platform();
+
+	xil_printf("\nMounting a volume...\n");
+	rc = pf_mount(&fatfs);
+	if(rc){
+		xil_printf("Failed mounting volume with rc = %d!\n", (int)rc);
+		return 1;
+	}
+
+	xil_printf("\nOpening a mp3 file...\n");
+	rc = pf_open("LINDSE~1.MP3");
+	if(rc){
+		xil_printf("Failed opening mp3 file with rc = %d!\n", (int)rc);
+		return 1;
+	}
+
+
+	struct mad_decoder decoder;
+
+	/* configure input, output, and error functions */
+	mad_decoder_init(
+		&decoder,
+		0 /* user data */,
+		input_fun,
+		0 /* header fun */,
+		0 /* filter fun */,
+		output_fun,
+		error_fun,
+		0 /* message fun */
+	);
+
+	/* start decoding */
+	int result = mad_decoder_run(&decoder, MAD_DECODER_MODE_SYNC);
+	if(result){
+		xil_printf("Decoding MP3 failed with rc = %d!", result);
+	}
+
+	/* release the decoder */
+	mad_decoder_finish(&decoder);
+
+	xil_printf("\nTest completed.\n");
+
+	return 0;
+}
+
+
+
+/*---------------------------------------------------------*/
+/* User Provided Timer Function for FatFs module           */
+/*---------------------------------------------------------*/
+
+DWORD get_fattime (void)
+{
+	return	  ((DWORD)(2010 - 1980) << 25)	/* Fixed to Jan. 1, 2010 */
+			| ((DWORD)1 << 21)
+			| ((DWORD)1 << 16)
+			| ((DWORD)0 << 11)
+			| ((DWORD)0 << 5)
+			| ((DWORD)0 >> 1);
+}
+
---
+++ SDK/SDK_Workspace/test_sdcard/src/main.c
@@ -0,0 +1,122 @@
+/*----------------------------------------------------------------------*/
+/* Petit FatFs sample project for generic uC  (C)ChaN, 2010             */
+/*----------------------------------------------------------------------*/
+
+#include <stdio.h>
+#include "pff.h"
+
+#include <stdio.h>
+#include "platform.h"
+#include "xparameters.h"
+#include "xil_types.h"
+
+
+#define die(rc) \
+	do{ \
+		xil_printf("Failed with rc=%d.\n", (int)rc); \
+		return 1; \
+	}while(0)
+
+
+
+/*-----------------------------------------------------------------------*/
+/* Program Main                                                          */
+/*-----------------------------------------------------------------------*/
+
+int main (void)
+{
+	FATFS fatfs;			/* File system object */
+	DIR dir;				/* Directory object */
+	FILINFO fno;			/* File information object */
+	WORD bw, br, i;
+	BYTE buff[64];
+	FRESULT rc;
+
+    init_platform();
+
+	xil_printf("\nMount a volume.\n");
+	rc = pf_mount(&fatfs);
+	if (rc) die(rc);
+
+	xil_printf("\nOpen a test file (message.txt).\n");
+	rc = pf_open("MESSAGE.TXT");
+	if (rc) die(rc);
+
+	xil_printf("\nType the file content.\n");
+	for (;;) {
+		rc = pf_read(buff, sizeof(buff), &br);	/* Read a chunk of file */
+		if (rc || !br) break;			/* Error or end of file */
+		for (i = 0; i < br; i++)		/* Type the data */
+			putchar(buff[i]);
+	}
+	if (rc) die(rc);
+
+#if _USE_WRITE
+	xil_printf("\nOpen a file to write (write.txt).\n");
+	rc = pf_open("WRITE.TXT");
+	if (rc) die(rc);
+
+	xil_printf("\nWrite a text data. (Hello world!)\n");
+	for (;;) {
+		rc = pf_write("Hello world!\r\n", 14, &bw);
+		if (rc || !bw) break;
+	}
+	if (rc) die(rc);
+
+	xil_printf("\nTerminate the file write process.\n");
+	rc = pf_write(0, 0, &bw);
+	if (rc) die(rc);
+#endif
+
+#if _USE_DIR
+	xil_printf("\nOpen root directory.\n");
+	rc = pf_opendir(&dir, "");
+	if (rc) die(rc);
+
+	xil_printf("\nDirectory listing...\n");
+	for (;;) {
+		rc = pf_readdir(&dir, &fno);	/* Read a directory item */
+		if (rc || !fno.fname[0]) break;	/* Error or end of dir */
+		if (fno.fattrib & AM_DIR)
+			xil_printf("   <dir>  %s\n", fno.fname);
+		else
+			xil_printf("%8d  %s\n", fno.fsize, fno.fname);
+	}
+	if (rc) die(rc);
+#endif
+
+	// Test reading speed.
+
+#if 1
+	xil_printf("\nOpening a 1MiB file...\n");
+	rc = pf_open("1MiB.dat");
+	BYTE buff2[1024];
+	if (rc) die(rc);
+	for (i = 0;; i++) {
+		rc = pf_read(buff2, sizeof(buff2), &br);	/* Read a chunk of file */
+		if (rc || !br) break;			/* Error or end of file */
+		//xil_printf("%d\n", i);
+	}
+	if (rc) die(rc);
+
+	xil_printf("\nTest completed.\n");
+#endif
+
+	return 0;
+}
+
+
+
+/*---------------------------------------------------------*/
+/* User Provided Timer Function for FatFs module           */
+/*---------------------------------------------------------*/
+
+DWORD get_fattime (void)
+{
+	return	  ((DWORD)(2010 - 1980) << 25)	/* Fixed to Jan. 1, 2010 */
+			| ((DWORD)1 << 21)
+			| ((DWORD)1 << 16)
+			| ((DWORD)0 << 11)
+			| ((DWORD)0 << 5)
+			| ((DWORD)0 >> 1);
+}
