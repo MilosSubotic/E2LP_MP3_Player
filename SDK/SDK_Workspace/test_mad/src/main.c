03daefac2f3beb3255288cfe012dffff4a2b7076
Milos Subotic <milos.subotic.sm@gmail.com>
2015-11-23 15:44:06 +0100

    MP3 player working nice.

--- SDK/SDK_Workspace/test_mad/src/main.c
+++ SDK/SDK_Workspace/test_mad/src/main.c
@@ -58,7 +58,7 @@
 
 #define NUM_OUT_BUFFS 2
 
-#define IN_BUFF_LEN 8192/2
+#define IN_BUFF_LEN 8192
 
 
 // Audio stuff.
@@ -129,12 +129,7 @@ static enum mad_flow input_fun(void *data, struct mad_stream *stream) {
 		// Say how much and where to read new bytes to fill up buffer up to the end.
 		to_read_start = in_buff + rest_len;
 		to_read_len = IN_BUFF_LEN-MAD_BUFFER_GUARD - rest_len;
-		xil_printf("rest_len = %d\n", rest_len);
-		xil_printf("rest_start[0] = 0x%02x\n", rest_start[0]);
 	}
-
-	xil_printf("in_chunk_cnt = %d\n", in_chunk_cnt);
-	xil_printf("consumed_read_bytes = %d\n", consumed_read_bytes);
 
 
 	clock_t start_read = clock();
@@ -153,10 +148,7 @@ static enum mad_flow input_fun(void *data, struct mad_stream *stream) {
 	u8* usefull_start = 0;
 	u8* usefull_end = 0;
 	// Search for sync word.
-	for(u8* p = in_buff; p < in_buff+IN_BUFF_LEN-MAD_BUFFER_GUARD-1; p++){/*
-		if(*p == 0xff && ((*(p+1) & 0xfc) == 0xfc)){
-			xil_printf("ADTS = %d\n", consumed_read_bytes + (p-in_buff));
-		}*/
+	for(u8* p = in_buff; p < in_buff+IN_BUFF_LEN-MAD_BUFFER_GUARD-1; p++){
 
 		// Possible sync word.
 		if(*p == 0xff && ((*(p+1) & 0xe0) == 0xe0)){
@@ -189,7 +181,6 @@ static enum mad_flow input_fun(void *data, struct mad_stream *stream) {
 					}
 					usefull_end = p+frm_len;
 
-					xil_printf("header = %d\n", consumed_read_bytes + (p-in_buff));
 				}
 			} // Else somethings is probably wrong.
 
@@ -201,8 +192,6 @@ static enum mad_flow input_fun(void *data, struct mad_stream *stream) {
 	consumed_read_bytes += usefull_end-in_buff;
 
 	int usefull_len = usefull_end-usefull_start;
-	xil_printf("usefull_len = %d\n", usefull_len);
-	xil_printf("usefull_end[0] = 0x%02x\n", usefull_end[0]);
 
 	// Move rest for MAD_BUFFER_GUARD torward end of buffer to have MAD_BUFFER_GUARD after usefull data.
 	u8* s = in_buff + IN_BUFF_LEN-MAD_BUFFER_GUARD - 1;
@@ -210,22 +199,14 @@ static enum mad_flow input_fun(void *data, struct mad_stream *stream) {
 	for(; s >= usefull_end; s--, d--){
 		*d = *s;
 	}
-	xil_printf("s[1] = 0x%02x\n", s[1]);
 	for(int i = MAD_BUFFER_GUARD-1; i >= 0; i--){
 		*d = 0;
 	}
 	rest_start = usefull_end + MAD_BUFFER_GUARD;
-	xil_printf("usefull_start-in_buff = %d\n", usefull_start-in_buff);
-	xil_printf("usefull_end-in_buff = %d\n", usefull_end-in_buff);
-	xil_printf("rest_start-in_buff = %d\n", rest_start-in_buff);
 
 	mad_stream_buffer(stream, usefull_start, usefull_len+MAD_BUFFER_GUARD);
 
 	in_chunk_cnt++;
-
-	if(in_chunk_cnt == 3){
-		return MAD_FLOW_STOP;
-	}
 
 	start_decode = clock();
 
@@ -369,8 +350,7 @@ static enum mad_flow error_fun(void *data, struct mad_stream *stream,
 
 	decode_clks += clock() - start_decode;
 
-//#if ENABLE_LOGS
-#if 1
+#if ENABLE_LOGS
 	xil_printf("decoding error 0x%04x (%s) at byte offset %d\n", stream->error,
 			mad_stream_errorstr(stream),
 			consumed_read_bytes + (stream->this_frame - in_buff));
